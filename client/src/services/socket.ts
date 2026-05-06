dd34c771ea5dd19fdc9c7419256957a5e63324be
Github-Sonali <guptasonali.4545@gmail.com>
2026-05-06 16:20:06 +0530

    fix: update API URLs to production

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://collab-notes-backend-efda.onrender.com", {
     withCredentials: true,
     autoConnect: false,
 });
